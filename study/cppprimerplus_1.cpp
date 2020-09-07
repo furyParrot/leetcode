f30605e595221da4ca6aa404b153f1e3e45e7482
furyParrot <418396075@qq.com>
2020-09-07 23:09:10 +0800

    2020年09月07日23:09:08

--- study/cppprimerplus_1.cpp
+++ study/cppprimerplus_1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<'\a';
+    int mm[] = {1233,1,1,11,1,1,1,2};
+    cout<<sizeof(mm)/sizeof(int);
+
+    cout<<"输入名字：\n";
+    char a[20];
+    cin.getline(a,10);
+    cout<<"你的名字是："<<a<<endl;
 }
